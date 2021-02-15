98490dae52336c78b95216c35df9efde457d9ea0
Ty <60036684+TyGillespie@users.noreply.github.com>
2021-02-15 11:20:31 -0700

    Removed unneeded code.

--- TyPadDoc.h
+++ TyPadDoc.h
@@ -10,7 +10,7 @@
 #endif // _MSC_VER > 1000
 
 
-class CTyPadDoc : public CDocument, , public CUndo
+class CTyPadDoc : public CDocument
 {
 protected: // create from serialization only
 	CTyPadDoc();
